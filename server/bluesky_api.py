724a318483eee2445f57e225a2a4fdd9b3d81887
Devin Gaffney <itsme@devingaffney.com>
2024-11-09 11:36:00 -0800

    updates

--- server/bluesky_api.py
+++ server/bluesky_api.py
@@ -1,10 +1,11 @@
+import os
 import re
 from atproto import Client, models, AtUri
 
 from atproto_client.models.app.bsky.embed.external import ViewExternal
 
 from server.logger import logger
-
+HOSTNAME = os.getenv("HOSTNAME")
 class BlueskyAPI:
     def __init__(self, username, password):
         self.username = username
@@ -16,9 +17,7 @@ class BlueskyAPI:
         #needs cursoring/pagination
         return self.client.com.atproto.repo.list_records({"collection": models.ids.AppBskyFeedGenerator, "repo": self.client.me.did})
     
-    def publish_feed(self, record_name, display_name, description, feed_did=None):
-        if not feed_did:
-            feed_did = f'did:web:{record_name}.{self.username}.hellofeed.cognitivesurpl.us'
+    def publish_feed(self, record_name, display_name, description, feed_did=f'did:web:{HOSTNAME}'):
         logger.info(f"calling publish_feed on {record_name} {display_name} {description} {feed_did}")
         response = self.client.com.atproto.repo.put_record(models.ComAtprotoRepoPutRecord.Data(
             repo=self.client.me.did,
