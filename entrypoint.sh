78895f2ce19de1ad9f7e3514d8f5922eb217db7a
Devin Gaffney <itsme@devingaffney.com>
2024-11-09 04:25:53 -0800

    reload by default

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 if [ "$1" = "api" ]; then
-    exec uvicorn "$APP_MODULE" --host "$HOST" --port "$PORT"
+    exec uvicorn "$APP_MODULE" --host "$HOST" --port "$PORT" --reload
 elif [ "$1" = "data-stream" ]; then
     export ENABLE_DATA_STREAM=true
-    exec uvicorn "$APP_MODULE" --host "$HOST" --port "$PORT"
+    exec uvicorn "$APP_MODULE" --host "$HOST" --port "$PORT" --reload
 else
     echo "Unknown command. Use 'api' or 'data-stream'."
     exit 1
